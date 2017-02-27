7b94689d4752afab58f025ac7af92f896dda5dec
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-02-27 18:13:34 +0530

    Tweaked the projecct files and other changes
    * This commit tweaks the project files to make it
    conform to the specified project guidelines. That involves removing the
    old project files and adding new ones
    * A little change corrects  make the text of a menu
    * This also remove the "First" folder that is not needed anymore
    * The .gitignore file is also added

---
+++ com.asgoc.add.ui/src/com/asgoc/add/ui/popup/actions/NewAction.java
@@ -0,0 +1,80 @@
+package com.asgoc.add.ui.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+
+import com.asgoc.add.ui.Activator;
+
+public class NewAction implements IObjectActionDelegate {
+
+	private Shell shell;
+	
+	/**
+	 * Constructor for Action1.
+	 */
+	public NewAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		/*MessageDialog.openInformation(
+			shell,
+			"Add",
+			"New Action was executed.");*/
+		try {
+			//get editor
+			IEditorPart editorPart = Activator.getDefault().getWorkbench()
+					.getActiveWorkbenchWindow().getActivePage()
+					.getActiveEditor();
+ 
+			if (editorPart instanceof AbstractTextEditor) {
+				String selectedText = null;
+				IEditorSite iEditorSite = editorPart.getEditorSite();
+				if (iEditorSite != null) {
+					ISelectionProvider selectionProvider = iEditorSite
+							.getSelectionProvider();
+					if (selectionProvider != null) {
+						ISelection iSelection = selectionProvider
+								.getSelection();
+						if (!iSelection.isEmpty()) {
+							selectedText = ((ITextSelection) iSelection)
+									.getText();
+							 MessageDialog.openInformation(
+							         shell,
+							         "Do Something Menu",
+							          "String: " +selectedText );
+						}
+					}
+				}
+ 
+			}
+		} catch (Exception e) {		}
+	}
+	
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
